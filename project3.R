cee40964e560db15c96975219b02a072e71e0316
KenHuynh0610 <nhan.huynh06@gmail.com>
2024-05-04 02:07:27 -0700

    Add files via upload

--- project3.R
+++ project3.R
@@ -2,16 +2,18 @@ library(DBI)
 library(RMySQL)
 library(dbplyr)
 
+tech_layoffs_df <- read_excel("C:/Users/Ken/Downloads/archive (4)/tech_layoffs.xlsx")
+
+tech_layoffs_df$retention_rate <- (tech_layoffs_df[, 10] / tech_layoffs_df[, 9]) * 100
+tech_layoffs_df
+
 #to connect to the db, input ur own db info
 con <- dbConnect(RMySQL::MySQL(), 
                  host = "localhost",
                  port = 3306,
                  user = "root",
-                 password = "Leavemealone998.",
+                 password = "",
                  db = "layoff_data")
 
+db
 dbListTables(con)
-
-dbListFields(con, "tech_layoffs")
-
-dbSendQuery()
