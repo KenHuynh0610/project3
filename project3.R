4fa8bef90ecc64ef1c55bea9a1daeea9edef90ce
KenHuynh0610 <nhan.huynh06@gmail.com>
2024-05-04 02:15:35 -0700

    Add files via upload

--- project3.R
+++ project3.R
@@ -1,8 +1,11 @@
 library(DBI)
 library(RMySQL)
 library(dbplyr)
+library(dplyr)
 
 tech_layoffs_df <- read_excel("C:/Users/Ken/Downloads/archive (4)/tech_layoffs.xlsx")
+
+mutate(tech_layoffs_df, retention_rate = (Company_Size_after_layoffs / Company_Size_before_Layoffs) * 100)
 
 tech_layoffs_df$retention_rate <- (tech_layoffs_df[, 10] / tech_layoffs_df[, 9]) * 100
 tech_layoffs_df
